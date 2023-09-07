49fefcf95698925a676b48a3cf63510b83a7ffc5
Rohan <rohanbansod60@gmail.com>
2023-09-08 01:26:11 +0530

    finished proj without local storage data

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ addButton.addEventListener("click", () => {
   const listElement = document.createElement("li");
 
   listElement.textContent = listValue;
-
+  if ((listValue = " ")) {
+    return;
+  }
   todoList.appendChild(listElement);
   inputField.value = " ";
+});
+todoList.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.remove();
+  }
 });
